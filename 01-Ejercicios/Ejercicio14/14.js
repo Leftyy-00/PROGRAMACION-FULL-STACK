1617f2f801bd84d65d47c03ab77ea4b94172b020
Leftyy-00 <rendongpgabrielc@gmail.com>
2026-06-11 18:41:37 -0300

    ofjgrsdwf

--- 01-Ejercicios/Ejercicio14/14.js
+++ 01-Ejercicios/Ejercicio14/14.js
@@ -11,18 +11,39 @@ let catButton = document.querySelector("#catbutton");
 let catbuttonGustar = document.querySelector("#catbuttonGustar");
 let catbuttonNoGustar = document.querySelector("#catbuttonNoGustar");
 let gatitos = document.querySelector("#catImage");
-/* 
 let contadorGustar = document.querySelector("#contadorgustar");
 let contadorNoGustar = document.querySelector("#contadornogustar");
-*/
+
+let gusta = 0;
+let noGusta = 0;
 
 catButton.addEventListener("click", async() => {
-    let response = await fetch("https://api.thecatapi.com/v1/images/search");
-    let data = await response.json();
-    
-    console.log("data", data);
+    try {
+        let response = await fetch("https://api.thecatapi.com/v1/images/search");
 
-    let imagen = data[0].url;
-    gatitos.src = imagen;
-    
+        if (!response.ok) {
+            throw new Error("Error al obtener la imagen");
+        }
+
+        let data = await response.json();
+        console.log("data", data);
+        gatitos.src = data[0].url;
+        idImagenActual = data[0].id;
+        console.log("ID de la imagen:", idImagenActual);
+
+    } catch (error) {
+
+        console.error(error);
+        alert("No se pudo cargar la imagen del gato.");
+    }
+});
+
+catbuttonGustar.addEventListener("click", () => {
+    gusta++;
+    contadorGustar.textContent = gusta;
+});
+
+catbuttonNoGustar.addEventListener("click", () => {
+    noGusta++;
+    contadorNoGustar.textContent = noGusta;
 });
